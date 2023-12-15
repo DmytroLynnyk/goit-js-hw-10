e63df76a701dcd629739cdac3a3e2b118d256ab7
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-12-15 18:10:50 +0200

    fetchBreeds with options rendering

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const catInfo = document.querySelector('.cat-info');
 
 loader.hidden = false;
 error.hidden = true;
-select.style.width = '150px';
+select.style.width = '180px';
 // select.addEventListener('change', onChange);
 
 function renderOptions(response) {
   const options = response.map(({ id, name }) => {
-    return `option value = "${id}" class="js-option">${name}</option>`;
+    return `<option value = "${id}" class="js-option">${name}</option>`;
   });
   options.unshift(
     '<option value="" disabled selected>Оберіть породу кішки...</option>'
