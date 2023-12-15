aee45cc8b2e269ac7f9c5328b00fee577d468f2a
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-12-15 15:09:54 +0200

    fetchBreeds plain no connect

---
+++ src/cat-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_L29djGtv1lF0NT0mClRarkdoGhfH7equBm4KwTFWhcS1rHpMlFAKS7AuvFIOg9PB';
+
+export function fetchBreeds() {
+  const instance = axios.create({
+    baseURL: 'https://api.thecatapi.com/v1/',
+    timeout: 1000,
+  });
+  return instance
+    .get('/breeds')
+    .then(response => {
+      if (response.status === 200) {
+        return response.status;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      return error;
+    });
+}
